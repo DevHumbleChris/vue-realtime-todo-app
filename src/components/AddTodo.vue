309a9bc7e7045dacb2335c025faf3fcd4ec41a7b
DevHumbleChris <dev.humblechris254@gmail.com>
2023-01-20 15:11:24 +0300

    finishing up

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -25,11 +25,13 @@ const addTodo = async () => {
 </script>
 
 <template>
-  <form @submit.prevent="addTodo" class="text-center">
-    <img :src="user?.photoURL" class="w-12 h-12 rounded-full border border-gray-400" />
-    <input type="text" name="price" id="price"
-      class="rounded-md border-gray-300 p-3 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-      placeholder="e.g learning vue.js" v-model="singleTodo" required>
+  <form @submit.prevent="addTodo" class="flex space-x-3 justify-center my-2">
+    <img :src="user?.photoURL" class="w-16 h-16 rounded-full border border-gray-400" />
+    <div>
+      <input type="text" name="price" id="price"
+        class="rounded-md border-gray-300 p-3 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        placeholder="e.g learning vue.js" v-model="singleTodo" required>
       <button type="submit" class="bg-indigo-500 p-2 mx-3 text-white rounded-md">ADD +</button>
+    </div>
   </form>
 </template>
--- src/components/TheTodo.vue
+++ src/components/TheTodo.vue
@@ -2,7 +2,7 @@
 import { TrashIcon, CheckBadgeIcon, XMarkIcon } from '@heroicons/vue/24/outline'
 import { computed } from 'vue'
 import { deleteDoc, doc, updateDoc } from '@firebase/firestore';
-import { db } from '@/firebaseConfig'
+import { db, auth } from '@/firebaseConfig'
 
 const props = defineProps({
     todo: Object
@@ -10,6 +10,10 @@ const props = defineProps({
 
 const todo = computed(() => {
     return props.todo
+})
+
+const user = computed(() => {
+    return auth.currentUser
 })
 
 const checkTodo = async (id) => {
@@ -26,9 +30,14 @@ const removeTodo = async (id) => {
 <template>
     <div class="mx-auto max-w-sm my-3 p-3 shadow border border-gray-400 rounded-xl">
         <div class="flex justify-between">
-            <img :src="todo?.user?.photoURL" class="w-12 h-12 rounded-full border border-gray-400" :alt="todo?.user.displayName"/>
-            <h2 :class="todo?.isCompleted && 'line-through text-green-600'">{{ todo?.title }}</h2>
-            <div class="flex flex-col items-center space-y-2">
+            <div class="flex space-x-4">
+                <img :src="todo?.user?.photoURL" class="w-16 h-16 rounded-full border border-gray-400" :alt="todo?.user.displayName"/>
+                <div>
+                    <h2>{{ todo?.user?.name }}</h2>
+                    <p :class="todo?.isCompleted? 'line-through text-green-600': 'text-gray-500'">{{ todo?.title }}</p>
+                </div>
+            </div>
+            <div v-if="todo.user.uid === user.uid" class="flex flex-col items-center space-y-2">
                 <CheckBadgeIcon v-if="!todo?.isCompleted" class="w-8 text-green-500 cursor-pointer" @click="checkTodo(todo?.id)"/>
                 <XMarkIcon v-else class="w-8 text-indigo-500 cursor-pointer" @click="checkTodo(todo?.id)"/>
                 <TrashIcon class="w-8 text-red-500 cursor-pointer" @click="removeTodo(todo?.id)" />
