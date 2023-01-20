d5e83eb2c068ddb39e8e24945389459fd68ab7b7
DevHumbleChris <dev.humblechris254@gmail.com>
2023-01-20 09:55:02 +0300

    added login route and component

--- src/router/index.js
+++ src/router/index.js
@@ -7,17 +7,23 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue')
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  if (requiresAuth) next({ name: 'login'})
+  else next()
 })
 
 export default router
